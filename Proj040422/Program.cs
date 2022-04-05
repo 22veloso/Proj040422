e4b5602e80ff9a07af4182e5c497fb15c6d33faa
22veloso <lucasveloso2205@gmail.com>
2022-04-04 22:17:10 -0300

    v0

---
+++ Proj040422/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Model;
+
+Console.WriteLine("Hello, World!");
+
+Client c1 = new Client()
+{
+
+    Id = 1,
+    Name = "teste",
+    Telephone = "16 9999 9999"
+
+};
+Client c2 = new Client()
+{
+
+    Id = 2,
+    Name = "teste2",
+    Telephone = "16 8888 8888"
+
+};
+Employee e1 = new Employee()
+{
+
+    Id = 1,
+    Name = "emp 1",
+    Telephone = "16 7777 7777"
+
+};
+Employee e2 = new Employee()
+{
+
+    Id = 2,
+    Name = "emp 2",
+    Telephone = "16 8888 8888"
+
+};
+
+
+List<Client> Clients =  new List<Client>();
+List<Employee> Employees = new List<Employee>(); 
+
+Clients.Add(c1);
+Clients.Add(c2);
+Employees.Add(e1);
+Employees.Add(e2);
+
+Hotel hotel = new Hotel()
+{
+    Client = Clients,
+Employee = Employees
+
+};
+
+Console.WriteLine(hotel.getClient());
+Console.WriteLine(hotel.getEmployee());
